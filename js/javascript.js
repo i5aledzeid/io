e1347ebfdf72e996f025c5a40955a985c164ad3c
Khaled Zeid <107222642+i5aledzeid@users.noreply.github.com>
2022-08-02 23:30:23 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,4 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-70447982-5');
+
 
 // calculator
 console.log("Javascript Calculator Made by Harsh Trivedi\nhttps://harsh98trivedi.github.io")
