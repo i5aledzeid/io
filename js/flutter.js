03bdc46f9836c367dbdda37b4d11ff58e174ed17
Khaled Zeid <107222642+i5aledzeid@users.noreply.github.com>
2022-08-03 00:09:56 +0200

    Add files via upload

--- js/flutter.js
+++ js/flutter.js
@@ -1,1 +1,12 @@
 alert("Update 1");
+
+var name = readLine("What's your name?");
+var favorite = readLine("What's your favorite programming language?");
+var language = readLine("What's your language?");
+
+function about_you() {
+    println("Hello, Mr. " + name);
+    println("Your favorite programming language is " + favorite);
+    println("You can speak " + language + " so, you will like a flutter.");
+    println("Flutter is framework, using Dart language.");
+}
